28715e3b7ac3e3c63271091ed912adfab0740d6b
Jiayisaac <77329155+Jiayisaac@users.noreply.github.com>
2026-09-05 18:20:42 +1000

    debugging

--- app.py
+++ app.py
@@ -41,11 +41,13 @@ def main():
     """Main loop for updating the Flame LED strip."""
     flame = Flame(pixels, LED_COUNT, CHANGE_COLOUR_PROBABILITY)
 
+    """
     web_thread = threading.Thread(
         target=run_web_server,
         daemon=True
     )
     web_thread.start()
+    """
 
     while True:
         button.update()
